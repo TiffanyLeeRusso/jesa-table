c4400361913f57952f72868890e296489acc9dff
TiffanyLeeRusso <tiffanyleerusso@gmail.com>
2026-03-19 17:57:06 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/projects/JesaTable/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
